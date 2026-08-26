5a1255de69395e96d5dbfc50de31ffd62c0e2a8c
Hadia Noor <hadiaurr88@gmail.com>
2026-08-26 21:30:26 +0500

    Update Excel metadata extraction for multiple files
    Refactored code to list and extract metadata from all .xlsx files in the current folder.

--- Excel (.xlsx)_metadata.py
+++ Excel (.xlsx)_metadata.py
@@ -1,36 +1,36 @@
-#pip install openpyxl ---> In terminal to install excel library
-#Why We Need It:
-#Python cannot read Excel files by default. We need this library to:
-#Open .xlsx files
-#Read their metadata
-#Extract author, title, dates, etc.
-from openpyxl import load_workbook 
-#Imports the load_workbook from the library , which will read excel files
-#Now we use the variable wb to open the file test.xlsx using our function , and so wb will contain all the sheets in the excel 
-#file, all the metadata in those sheets asw
-wb = load_workbook("test.xlsx")
-#wb is a variable that stores the workbook object 
-props = wb.properties
-#probs is a varibale that stores the wb properties like creation time, title, subject, created etc 
-creator = props.creator #pretty self-explanatory
-print("Creator:", creator)
-creator = props.creator 
-if props.creator:
-    creator = props.creator
-else:
-    creator = "Unknown"
-  if props.title:
-    title = props.title
-else:
-    title = "Unknown"
-print("Title:", title) 
-if props.created:
-    created = props.created
-else:
-    created = "Unknown"
-  print("Created:", created)
-sheets = len(wb.sheetnames) 
-# wb.sheetnames ---> gets a list of all sheet names 
-# len--> gets the count of how many sheets there are 
-# sheets stores the count 
-
+import os #---> To import the os to load all files
+from openpyxl import load_workbook
+folder = "." #---> current folder
+#So the thing is that I couldn't have .xlsx type of files in my main folder , so pardon me for that. 
+#I don't know if I made my point come across. 
+#To get all the files in the folder.
+all_files = os.listdir(folder)#A list of all files
+xlsx_files = [] #Again a list
+for file in all_files: #---> To get all files in all_files, which now contains all the files of our type in the folder
+    if file.endswith(".xlsx"):
+        xlsx_files.append(file) #-->Appended to the list 
+print("Found", len(xlsx_files), "Excel files")
+print("=" * 40)
+for xlsx in xlsx_files:
+    filepath = os.path.join(folder, xlsx)
+    wb = load_workbook(filepath)
+    props = wb.properties
+    if props.creator:
+        creator = props.creator
+    else:
+        creator = "Unknown"
+    if props.title:
+        title = props.title
+    else:
+        title = "Unknown"
+    if props.created:
+        created = props.created
+    else:
+        created = "Unknown"
+    sheets = len(wb.sheetnames)
+    print("File:", xlsx)
+    print("  Creator:", creator)
+    print("  Title:", title)
+    print("  Created:", created)
+    print("  Sheets:", sheets)
+    print("-" * 30)
